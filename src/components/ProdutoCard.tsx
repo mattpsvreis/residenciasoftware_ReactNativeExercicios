c0c12b7c66c2cdc0fdc03bacda1a9b68769b0191
mattpsvreis <mateuspsvreis@gmail.com>
2022-06-29 02:06:20 -0300

    Formatação BRL

--- src/components/ProdutoCard.tsx
+++ src/components/ProdutoCard.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Text, Image, Card } from 'react-native-elements';
+import { Text, Card } from 'react-native-elements';
 
 export default function ProdutoCard(props: any) {
-    const formatBRL = (value: any) => {
-        const options = { style: "currency", currency: "BRL"};
-        return value.toLocaleString("pt-BR", options);
-    }
 
     const image = `${props.produto.imagemProduto}`;
 
@@ -14,13 +10,13 @@ export default function ProdutoCard(props: any) {
         <TouchableOpacity onPress={() => console.log(`${props.produto.nomeProduto} foi clicado(a)`)}>
             <Card containerStyle={props.styles.produtoContainer}>
                 <Card.Image
-                    source={{uri: image}}
+                    source={{ uri: image }}
                     style={props.styles.produtoContainerImage}
                     width={undefined}
                     height={undefined}
                 />
                 <Card.Title style={props.styles.textCardTitle}>{props.produto.nomeProduto}</Card.Title>
-                <Text style={props.styles.textCardValue}>{formatBRL(`${props.produto.precoProduto}`)}</Text>
+                <Text style={props.styles.textCardValue}>R$ {parseFloat(props.produto.precoProduto).toFixed(2).replace('.',',')}</Text>
             </Card>
         </TouchableOpacity>
     )
